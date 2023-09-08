da5cfba9c2817c1b1881995cfc12672650a0d2e3
Toshiya Saito <saitou@ialab.cs.tsukuba.ac.jp>
2023-09-08 17:27:23 +0900

    npm pack するディレクトリを作成

---
+++ package/test.js
@@ -0,0 +1,6 @@
+const mod = require(".");
+
+let n = 0;
+console.log((n = mod.increment(n)));
+console.log((n = mod.increment(n)));
+console.log((n = mod.increment(n)));
---
+++ package/test.md
@@ -0,0 +1,5 @@
+# test
+
+[ここ](https://github.com/abcd-ts/napi-sample/actions) の、最新のワークフローから、アーキテクチャに対応した成果物をダウンロードし、同じディレクトリに置く。
+
+`node test.js` が動作したらOK。
